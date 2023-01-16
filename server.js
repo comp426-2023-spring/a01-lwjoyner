3a8fc997d8076137ac0d9b28d14e4ede1a7cc425
Logan Joyner <ljoyner@unc.edu>
2023-01-15 19:08:36 -0500

    Did the port const the right wa.y

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 const minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const arg = minimist(process.argv.slice(2))['port'];
+const args = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = (arg == null ? 3000 : arg);
+const port = args.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
